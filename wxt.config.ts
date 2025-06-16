b7fd6c63acfaa553f6f7df45064a007bc5c8074e
ritikprajapat21 <ritikprajapati084@gmail.com>
2025-06-16 19:38:33 +0530

    Added assets and updated description

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,5 +5,9 @@ export default defineConfig({
 	modules: ["@wxt-dev/module-react"],
 	manifest: {
 		permissions: ["history", "storage"],
+		name: "Smart History Cleaner",
+		short_name: "History Cleaner",
+		description:
+			"Effortlessly manage and delete your Chrome Browse history with custom regex patterns. Enhance privacy, remove unwanted URLs, and keep your history clean. Supports automatic and selective history cleanup.",
 	},
 });
